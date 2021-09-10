760bc60a95047cc70bc161c99cdd1ff6a26c5755
pgrilo92 <pgrilo92@hotmail.com>
2021-09-10 10:12:56 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-page">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+        Button
+      </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
